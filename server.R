69d7e05d890ec28ac01a5234c11b70309be36b7b
barkerh <51452646+barkerh@users.noreply.github.com>
2019-07-30 17:04:37 -0400

    Update server.R

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ library(modelr)
 library(rgl)
 library(ggplot2)
 
-vehicles <- read.csv("VehiclesApp/blob/master/2018VehiclesDatasetrevised.csv", header = T)
+vehicles <- read.csv("2018VehiclesDatasetrevised.csv", header = T)
 
 df <- data.frame(vehicles[5:8], vehicles[10], vehicles[12], vehicles[14:18])
 # Define server logic required to draw a scatterplot
